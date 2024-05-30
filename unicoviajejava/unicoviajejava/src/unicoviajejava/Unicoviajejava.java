7c69397ff61524d71a4d1a32bbae38a43e6709b5
cristian adso <romerocristian563@gmail.com>
2024-05-29 20:50:56 -0500

    mi primer commit

---
+++ unicoviajejava/unicoviajejava/src/unicoviajejava/Unicoviajejava.java
@@ -0,0 +1,9 @@
+package unicoviajejava;
+
+public class Unicoviajejava {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola cristian ");
+    }
+}
